4bea1237c4dc1251073ec7674badb7e1bc7d1ce6
root <root@LAPTOP-K9SIBRAJ.localdomain>
2025-12-04 20:42:41 +0300

    pairing

---
+++ calculator/pairing.js
@@ -0,0 +1,24 @@
+const pairing = (pair, ip) => {
+  const {power, mana} = ip;
+
+  const perfect_pair_power = power * 3;
+  const improved_pair_power = power * 2;
+  const minimal_pair_power = power * 0.75;
+
+  const improved_pair_mana = mana + (mana / 2);
+  const minimal_pair_mana = mana + (mana * 2);
+
+  if (pair === "perfect") {
+    ip.power = perfect_pair_power
+  } else if (pair === 'improved') {
+    ip.power = improved_pair_power
+    ip.mana = improved_pair_mana
+  } else if (pair === 'minimal') {
+    ip.power = minimal_pair_power
+    ip.mana = minimal_pair_mana
+  }
+
+  return ip;
+};
+
+export default pairing;
---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+export default class CalculatorIP extends React.Component {
+  render() {
+    return (<div><h1>Слайм лоликонщик</h1></div>)
+  }
+}
+
+ReactDOM.createRoot(document.getElementById('app')).render(<CalculatorIP />)
